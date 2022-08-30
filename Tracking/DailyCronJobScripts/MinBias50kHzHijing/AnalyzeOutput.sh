ff712b5d93a7bb0d065ceb63cee65380fb59ed07
Joe Osborn <osbornjd@ornl.gov>
2022-08-30 16:01:36 -0400

    fix repo bug

--- Tracking/DailyCronJobScripts/MinBias50kHzHijing/AnalyzeOutput.sh
+++ Tracking/DailyCronJobScripts/MinBias50kHzHijing/AnalyzeOutput.sh
@@ -10,10 +10,10 @@ YESTERDAY_DIR="/sphenix/sim/sim01/sphnxpro/TrackingDailyBuild/$(date +"%"Y)/$(da
 cd $HOME_DIR
 pwd
 # creates time.txt file with aggregated timing information
-~/git/analysis/Tracking/BenchmarkingTools/TimingTools/parseTimers.sh $HOME_DIR/ $HOME_DIR/
+~/git/DataMonitoring/Tracking/TimingTools/parseTimers.sh $HOME_DIR/ $HOME_DIR/
 
 #creates timingoutfile.root containing histogramed timing information
-root -b -q -l ~/git/analysis/Tracking/BenchmarkingTools/TimingTools/AnalyzeTime.C\(\"$HOME_DIR/time.txt\",\"$HOME_DIR/timingoutfile.root\"\)
+root -b -q -l ~/git/DataMonitoring/Tracking/TimingTools/AnalyzeTime.C\(\"$HOME_DIR/time.txt\",\"$HOME_DIR/timingoutfile.root\"\)
 
 #Aggregate job file output
 cd $HOME_DIR
@@ -25,15 +25,15 @@ hadd -k MinBias50kHzHijingDailyBuild_g4svtx.root MinBias50kHzHijing_dailybuild_*
 # run qa macros and save histograms in home dir
 root -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/MvtxQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MvtxComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/InttQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/InttComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/InttQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/InttComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/TpcQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/TpcComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/TpcQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/TpcComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/TpotQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/TpotComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/TpotQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/TpotComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/TrackingQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/TrackingComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/TrackingQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/TrackingComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/VertexingQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/VertexingComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/VertexingQA.C\(\"$YESTERDAY_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/MinBias50kHzHijingDailyBuild_qa.root\",\"$HOME_DIR/VertexingComparisons.root\"\)
 
 #Compile the QA results and move to the top working directory for the day
 hadd MinBias50kHzHijingQA.root timingoutfile.root *Comparisons.root
