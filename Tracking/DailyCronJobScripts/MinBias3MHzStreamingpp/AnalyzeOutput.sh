de17f459c76668fd5d804428bb7bc765b10a6220
Joe Osborn <osbornjd@ornl.gov>
2022-08-30 17:03:49 -0400

    update paths and copy output to html area

--- Tracking/DailyCronJobScripts/MinBias3MHzStreamingpp/AnalyzeOutput.sh
+++ Tracking/DailyCronJobScripts/MinBias3MHzStreamingpp/AnalyzeOutput.sh
@@ -10,10 +10,10 @@ YESTERDAY_DIR="/sphenix/sim/sim01/sphnxpro/TrackingDailyBuild/$(date +"%"Y)/$(da
 cd $HOME_DIR
 pwd
 # creates time.txt file with aggregated timing information
-~/git/analysis/Tracking/BenchmarkingTools/TimingTools/parseTimers.sh $HOME_DIR/ $HOME_DIR/
+~/git/DataMonitoring/Tracking/TimingTools/parseTimers.sh $HOME_DIR/ $HOME_DIR/
 
 #creates timingoutfile.root containing histogramed timing information
-root -b -q -l ~/git/analysis/Tracking/BenchmarkingTools/TimingTools/AnalyzeTime.C\(\"$HOME_DIR/time.txt\",\"$HOME_DIR/timingoutfile.root\"\)
+root -b -q -l ~/git/DataMonitoring/Tracking/TimingTools/AnalyzeTime.C\(\"$HOME_DIR/time.txt\",\"$HOME_DIR/timingoutfile.root\"\)
 
 #Aggregate job file output
 cd $HOME_DIR
@@ -25,17 +25,22 @@ hadd -k MinBias3MHzStreamingppDailyBuild_g4svtx.root MinBias3MHz_pythia8_Charm_d
 # run qa macros and save histograms in home dir
 root -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/MvtxQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MvtxComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/InttQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/InttComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/InttQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/InttComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/TpcQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/TpcComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/TpcQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/TpcComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/TpotQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/TpotComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/TpotQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/TpotComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/TrackingQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/TrackingComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/TrackingQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/TrackingComparisons.root\"\)
 
-root  -l -b -q ~/git/analysis/Tracking/BenchmarkingTools/PhysicsPerformanceTools/VertexingQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/VertexingComparisons.root\"\)
+root  -l -b -q ~/git/DataMonitoring/Tracking/PhysicsPerformanceTools/VertexingQA.C\(\"$YESTERDAY_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/MinBias3MHzStreamingppDailyBuild_qa.root\",\"$HOME_DIR/VertexingComparisons.root\"\)
 
 #Compile the QA results and move to the top working directory for the day
 hadd MinBias3MHzStreamingppQA.root timingoutfile.root *Comparisons.root
 mv MinBias3MHzStreamingppQA.root ../
 mv MinBias3MHzStreamingppDailyBuild_g4svtx.root ../
+
+cd ../
+                                 
+cp MinBias3MHzStreamingppQA.root /sphenix/WWW/subsystem/DataMonitoring/Tracking/3MHzMinBiasppToday.root
+cp $YESTERDAY_DIR/../MinBias3MHzStreamingppQA.root /sphenix/WWW/subsystem/DataMonitoring/Tracking/3MHzMinBiasppYesterday.root
